6f54037fbd2cb782265891d1bb2f393f59ba4727
khactam94 <khac.tam.94@gmail.com>
2016-11-30 16:01:34 +0700

    first version

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>How to</title>
+	<link rel="stylesheet" href="lib/css/style.css">
+	<script src="lib/js/script.js"></script>
+</head>
+<body>
+<div class="menu">
+	<ul>
+		<li><a href="#" >Thông báo</a></li>
+		<li><a href="#" >Chương trình đào tạo</a></li>
+		<li><a href="#" >Kế hoạch</a></li>
+		<li><a href="#" >Tra cứu sinh viên</a>
+			<ul class="sub-menu">
+				<li><a href="#" >SV đang học</a></li>
+				<li><a href="#" >SV ngừng học</a></li>
+				<li><a href="#" >SV bị hủy đăng kí do chây ỳ đóng học phí</a></li>
+				<li><a href="#" >SV lười học thông minh</a></li>
+			</ul>
+		</li>
+		<li><a href="#" >Quy chế & Quy định</a></li>
+		<li><a href="#" >Hướng dẫn</a></li>
+	</ul>
+</div>
+</body>
+</html>
